0b70a59f704ff761645f949e8b1705e1c77ff67e
Dana Truong <tramtramphotos@gmail.com>
2022-08-03 19:50:31 -0500

    Add the for assignment and self evaluation

--- for.js
+++ for.js
@@ -1,22 +1,51 @@
 // As you complete each challenge, RUN THE CODE to ensure it works. You can do this by
 // running `node <filepath>`. If you are in this directory, you'd run `node for.js`.
-  
+
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
 
-
+for (var i = 25; i <= 75; i++) {
+  if (i % 2 === 0) {
+    console.log("1." + i)
+  }
+}
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
+
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+
+for (var i = 0; i < weather_options.length; i++) {
+  console.log(`It is ${weather_options[i]} today!`)
+}
 
 
 // Challenge 3
-// Use a for statement to log to the console only the words that are exactly 3 characters in length
+// Use a for statement to log to the console only the words
+// that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
-// 📹 Record yourself live-coding and talking through any ONE of the challenges. 
-  // The recording doesn't have to be the first time you are solving the problem. 
-  // Use technical vocabulary and explain each part to demonstrate your understanding! 
-  // Share the link to your video in your small group channel.
+for (var i = 0; i < words.length; i++) {
+  if (words[i].length === 3) {
+    console.log(words[i])
+  }
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 📹 Record yourself live-coding and talking through any ONE of the challenges.
+  // The recording doesn't have to be the first time you are solving the problem.
+  // Use technical vocabulary and explain each part to demonstrate your understanding!
+  // Share the link to your video in your small group channel.
--- selfEvaluation.md
+++ selfEvaluation.md
@@ -1,6 +1,21 @@
 # Self Evaluation
 
 - How confident do you feel in your understanding and fluency with the `for` statement?
+
+It's a bit of a doozie, but I feel fairly confident. The syntax is still confusing and I always have to
+tweak it before it returns the correct value.
+
 - This was a much more advanced concept than anything you've done in Mod 0, and the exercises required you to push yourself to apply other background knowledge or learn things outside of the curriculum - how did that feel? Did you have productive struggle? Unproductive struggle?
+
+I had a productive struggle with this. Just from first impression alone made me panicky, but if I examine
+the issue, break it into smaller pieces, work on bits one at a time, I manage to complete the exercise.
+I also had to research the length method for the final problem because I didn't know the method to count the
+character length of a string at the top of my head.
+
 - What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that?
+
+I still need to practice and examine the way for loops and if statements are structured when an array is involved. It's not very intuitive for me and I need to further work on that.
+
 - What questions do you still have? How will you get the answers you need?
+
+No other questions.
